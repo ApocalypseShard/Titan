8f044fa8a7fd3799e52a18d2a87321b68817a8d7
KarolyS <user@server>
2012-11-15 23:43:24 -0800

    Initial Setup
    Initial setup.

---
+++ KarolyProtoCode/WPF_FBConnect_Proto/WPF_FBConnect_Proto/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+
+namespace WPF_FBConnect_Proto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        /* Various Fields to which GUI supports access
+         * Auth: string oauthTestString = "https://graph.facebook.com/oauth/authorize?type=user_agent&client_id=211505718975007&redirect_uri=https%3A%2F%2Fwww.facebook.com/connect/login_success.html&scope=read_friendlists,read_mailbox";
+         * De-auth: "https://graph.facebook.com/me/permissions?method=delete"
+         * View Friends "https://graph.facebook.com/me/friends?accesstoken="
+         * Profile Feed "https://graph.facebook.com/me/
+         * Likes "https://graph.facebook.com/me/likes?accesstoken="
+         * Photos "https://graph.facebook.com/me/albums?accesstoken="
+         * Profile Pic "https://graph.facebook.com/me/picture?=accesstoken="
+         */
+
+        private string appID = "211505718975007";
+        private string redirectURI = "https://www.facebook.com/connect/login_success.html";
+        string responseType = "token";
+        string display = "popup";
+        string extendedPerms = "read_friendlists, read_mailbox";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void auth_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            string oauthTestString = "https://graph.facebook.com/oauth/authorize?type=user_agent&client_id=211505718975007&redirect_uri=https%3A%2F%2Fwww.facebook.com/connect/login_success.html&scope=read_friendlists,read_mailbox";
+
+            HttpWebRequest httpWebReq = (HttpWebRequest)WebRequest.Create(oauthTestString);
+
+            HttpWebResponse response = (HttpWebResponse)httpWebReq.GetResponse();
+
+            auth_Lbl.Content = response.ToString();
+
+        }
+
+        private void viewFriends_Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deAuth_Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ KarolyProtoCode/WPF_FBConnect_Test/WPF_FBConnect_Test/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Web;
+
+
+namespace WPF_FBConnect_Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string accessToken;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void testauth_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            string appID = "211505718975007";
+            string redirectURI = "https://www.facebook.com/connect/login_success.html";
+            string responseType = "token";
+            string display = "popup";
+            string extendedPerms = "read_friendlists,read_mailbox";
+
+            //Failed
+            //"https://graph.facebook.com/oauth/authorize?type=user_agent&client_id=116122545078207&redirect_uri=http%3A%2F%2Fbenbiddington.wordpress.com&scope=user_photos,email,user_birthday,user_online_presence"
+
+            //Web browser test success
+            //https://graph.facebook.com/oauth/authorize?type=user_agent&client_id=211505718975007&redirect_uri=https%3A%2F%2Fwww.facebook.com/connect/login_success.html&scope=read_friendlists,read_mailbox
+
+            string oauthTestString = "https://graph.facebook.com/oauth/authorize?type=user_agent&client_id=211505718975007&redirect_uri=https%3A%2F%2Fwww.facebook.com/connect/login_success.html&scope=read_friendlists,read_mailbox";
+            
+            webBrowser1.Navigate(oauthTestString);
+        }
+
+        private void requestFriendsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string getFriendsUrl = "https://graph.facebook.com/me/friends?access_token=" + accessToken;
+            webBrowser1.Navigate(getFriendsUrl);
+        }
+
+        private void webBrowser1_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            
+            string[] url = webBrowser1.Source.AbsoluteUri.Split('#');
+
+            if (url.Length > 1)
+            {
+                string[] prms = url[1].Split('=');
+                accessToken = prms[1].Split('&')[0];
+
+                accessToken_Lbl.Content = accessToken;//webBrowser1.Source.AbsoluteUri;
+            }
+
+
+        }
+
+       
+    }
+}
